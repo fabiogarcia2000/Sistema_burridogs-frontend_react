825e0dbed2124ed1b1742d87d465e064e84f70dd
Fabio Garcia <fabio.garcia@unah.hn>
2022-10-18 21:50:23 -0600

    formulario sucursal

---
+++ src/components/administracion_pos/sucursales/EditarSucursal.jsx
@@ -0,0 +1,276 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { useParams } from "react-router-dom";
+
+const EditarSucursal = () => {
+  const { id } = useParams();
+  const { type } = useParams();
+  console.log(id);
+  console.log(type);
+
+  //Configurar los hooks
+  const [formularioEnviado, setFormularioEnviado] = useState(false);
+
+
+  if (type === "new") {
+    console.log("Crear Nuevo registro");
+  } else if (type === "edit") {
+    console.log("Editar un registro");
+  };
+
+  return (
+    <div className="container">
+      <Formik
+        //valores iniciales
+
+        initialValues={{
+          id: id,
+          descripcion: "",
+          direccion: "",
+          telefono: "",
+          rtn: "",
+          centroCosto: "",
+          estado: "Activo",
+        }}
+        //Funcion para validar
+        validate={(valores) => {
+          let errores = {};
+
+          // Validacion id
+          if (!valores.id) {
+            errores.id = "Por favor ingresa un id";
+          } else if (!/^^[0-9]+$/.test(valores.id)) {
+            errores.id = "El id solo puede contener números espacios";
+          }
+
+          // Validacion descripción
+          if (!valores.descripcion) {
+            errores.descripcion = "Por favor ingresa una descripción";
+          }
+
+          // Validacion dirección
+          if (!valores.direccion) {
+            errores.direccion = "Por favor ingresa una dirección";
+          }
+
+          // Validacion teléfono
+          if (!valores.telefono) {
+            errores.telefono = "Por favor ingresa un teléfono";
+          }
+
+          // Validacion teléfono
+          if (!valores.telefono) {
+            errores.telefono = "Por favor ingresa un teléfono";
+          }
+
+          // Validacion rtn
+          if (!valores.rtn) {
+            errores.rtn = "Por favor ingresa un rtn";
+          } else if (!/^^[0-9]+$/.test(valores.rtn)) {
+            errores.id = "El rtn solo puede contener números";
+          }
+
+          // Validacion ID de Centro de Costo
+          if (!valores.centroCosto) {
+            errores.centroCosto = "Por favor ingresa un ID de Centro de Costo";
+          }
+
+          // Validacion estado
+          if (!valores.estado) {
+            errores.estado = "Por favor ingresa un estado";
+          }
+
+          return errores;
+        }}
+        onSubmit={(valores, { resetForm }) => {
+          //Enviar los datos (petición Post)
+          console.log("Formulario enviado");
+
+          resetForm();
+          setFormularioEnviado(true);
+        }}
+      >
+        {({ errors }) => (
+          <Form className="formulario">
+            <h3 className="mb-3">Editar Sucursal</h3>
+            <div className="row g-3">
+              <div className="col-sm-6">
+                <div className="mb-3">
+                  <label htmlFor="idSucursal" className="form-label">
+                    ID de Sucursal:
+                  </label>
+                  <Field
+                    type="text"
+                    className="form-control"
+                    id="idSucursal"
+                    name="id"
+                    placeholder="ID de Sucursal..."
+                  />
+
+                  <ErrorMessage
+                    name="id"
+                    component={() => <div className="error">{errors.id}</div>}
+                  />
+                </div>
+              </div>
+
+              <div className="col-sm-6">
+                <div className="mb-3">
+                  <label htmlFor="descripcionSucursal" className="form-label">
+                    Descripción:
+                  </label>
+                  <Field
+                    type="text"
+                    className="form-control"
+                    id="descripcionSucursal"
+                    name="descripcion"
+                    placeholder="Descripcion..."
+                  />
+
+                  <ErrorMessage
+                    name="descripcion"
+                    component={() => (
+                      <div className="error">{errors.descripcion}</div>
+                    )}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="row g-3">
+              <div className="col-sm-6">
+                <div className="mb-3">
+                  <label htmlFor="direccionSucursal" className="form-label">
+                    Dirección:
+                  </label>
+                  <Field
+                    type="text"
+                    className="form-control"
+                    id="direccionSucursal"
+                    name="direccion"
+                    placeholder="Dirección..."
+                  />
+
+                  <ErrorMessage
+                    name="direccion"
+                    component={() => (
+                      <div className="error">{errors.direccion}</div>
+                    )}
+                  />
+                </div>
+              </div>
+
+              <div className="col-sm-6">
+                <div className="mb-3">
+                  <label htmlFor="telefonoSucursal" className="form-label">
+                    Teléfono:
+                  </label>
+                  <Field
+                    type="text"
+                    className="form-control"
+                    id="telefonoSucursal"
+                    name="telefono"
+                    placeholder="Teléfono..."
+                  />
+
+                  <ErrorMessage
+                    name="telefono"
+                    component={() => (
+                      <div className="error">{errors.telefono}</div>
+                    )}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="row g-3">
+              <div className="col-sm-6">
+                <div className="mb-3">
+                  <label htmlFor="rtnSucursal" className="form-label">
+                    RTN:
+                  </label>
+                  <Field
+                    type="text"
+                    className="form-control"
+                    id="rtnSucursal"
+                    name="rtn"
+                    placeholder="RTN..."
+                  />
+
+                  <ErrorMessage
+                    name="rtn"
+                    component={() => <div className="error">{errors.rtn}</div>}
+                  />
+                </div>
+              </div>
+
+              <div className="col-sm-6">
+                <div className="mb-3">
+                  <label htmlFor="centroCostoSucursal" className="form-label">
+                    ID de Centro de Costo:
+                  </label>
+                  <Field
+                    type="text"
+                    className="form-control"
+                    id="centroCostoSucursal"
+                    name="centroCosto"
+                    placeholder="ID de Centro Costo..."
+                  />
+
+                  <ErrorMessage
+                    name="centroCosto"
+                    component={() => (
+                      <div className="error">{errors.centroCosto}</div>
+                    )}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="row g-3">
+              <div className="col-md-4 mb-3">
+                <label htmlFor="estadoSucursal" className="form-label">
+                  Estado:
+                </label>
+                <Field
+                  as="select"
+                  className="form-select"
+                  id="estadoSucursal"
+                  name="estado"
+                >
+                  <option value="Activo">Activo</option>
+                  <option value="Inactivo">Inactivo</option>
+                </Field>
+
+                <ErrorMessage
+                  name="estado"
+                  component={() => <div className="error">{errors.estado}</div>}
+                />
+              </div>
+              <hr />
+            </div>
+
+            <button className="btn btn-success mb-3 me-2" type="submit">
+              Guardar
+            </button>
+            <Link
+              to="/mostrarsucursales"
+              type="button"
+              className="btn btn-danger mb-3 me-2"
+            >
+              Cancelar
+            </Link>
+
+            {/*Mostrar mensaje de exito al enviar formulario */}
+            {formularioEnviado && (
+              <p className="exito">Formulario enviado con exito!</p>
+            )}
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default EditarSucursal;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { useGlobalState } from "../Layout/responseClass";
 import Header from "../Layout/Header";
-import Sidebar from "../Layout/Sidebar"
+import Sidebar from "../Layout/Sidebar";
 import Footer from "../Layout/Footer";
 
 import Home from "../pages/Home";
 import Productos from "../pages/inventario/Productos";
+import MostrarSucursales from "../components/administracion_pos/sucursales/MostrarSucursales";
+import CrearSucursal from "../components/administracion_pos/sucursales/CrearSucursal";
+import EditarSucursal from "../components/administracion_pos/sucursales/EditarSucursal";
 
 function Rutas() {
   const [main_class] = useGlobalState("main_class");
@@ -13,12 +16,22 @@ function Rutas() {
   return (
     <BrowserRouter>
       <Header />
-      <Sidebar/>
+      <Sidebar />
       <main id="main" className={main_class}>
         <section className="section dashboard">
           <Routes>
             <Route path="/" element={<Home />} />
+            {/*Rutas Administración POS*/}
+            <Route path="/mostrarsucursales" element={<MostrarSucursales />} />
+            <Route path="/crearsucursales" element={<CrearSucursal />} />
+            <Route path='/editarsucursal/:id/:type' element={<EditarSucursal />} />
             <Route path="/productos" element={<Productos />} />
+            {/*Rutas inventario*/}
+
+            {/*Rutas contabilidad*/}
+
+            {/*Rutas reportes*/}
+
             <Route path="*" element={<h1>404</h1>} />
           </Routes>
         </section>
