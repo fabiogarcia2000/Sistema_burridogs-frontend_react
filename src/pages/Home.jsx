4c49bd819e115bafeba7ff26f028401fbbb9bb15
Jonathan Lopez <101952817+JonatLopezz@users.noreply.github.com>
2022-11-12 05:57:24 -0600

    Pequeña mejora para página Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import logoEmpresa from "../assets/img/logo1.png";
 
 function Home() {
   return (
-    <h1>Contenido</h1>
+    <div>
+      <h1 id="Bienvenido">Bienvenido Al Sistema POS Burridogs</h1>
+      <img id="LogoEmpresa" src={logoEmpresa} alt="Logo Empresa" />
+    </div>
   );
 }
 
