c18be6cd0992701453f86a5747124c9b68af6751
Jonathan Lopez <101952817+JonatLopezz@users.noreply.github.com>
2022-11-11 21:44:43 -0600

    arreglo de funciones

--- src/utils/cambiarAMayusculas.js
+++ src/utils/cambiarAMayusculas.js
@@ -26,3 +26,71 @@ export function cambiarAMayusculasDirección(valores) {
     console.log(error);
   }
 }
+
+//CONTABILIDAD----------------------------------------------------------------
+export function cambiarAMayusculasCodigo(valores) {
+  try {
+    let texto = valores.codigo_cuenta;
+    valores.codigo_cuenta = texto.toUpperCase();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export function cambiarAMayusculasNombreCuenta(valores) {
+  try {
+    let texto = valores.nombre_cuenta;
+    valores.nombre_cuenta = texto.toUpperCase();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export function cambiarAMayusculasNombreCategoria(valores) {
+  //función convertir a mayuscula descripción corta
+
+  try {
+    let texto = valores.nombre_categoria;
+    valores.nombre_categoria = texto.toUpperCase();
+  } catch (error) {
+    console.log(error);
+  }
+}
+//SUBCUENTA
+export function cambiarAMayusculasNombreSubcuenta(valores) {
+  try {
+    let texto = valores.nombre_subcuenta;
+    valores.nombre_subcuenta = texto.toUpperCase();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//ESTADO
+export function cambiarAMayusculasTipoEstado(valores) {
+  try {
+    let texto = valores.tipo_estado;
+    valores.tipo_estado = texto.toUpperCase();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//PERIODO CONTABLE
+export function cambiarAMayusculasDescripcionPeriodo(valores) {
+  try {
+    let texto = valores.descripcion_periodo;
+    valores.descripcion_periodo = texto.toUpperCase();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export function cambiarAMayusculasNombreUsuario(valores) {
+  try {
+    let texto = valores.nombre_usuario;
+    valores.nombre_usuario = texto.toUpperCase();
+  } catch (error) {
+    console.log(error);
+  }
+}
