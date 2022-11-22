824743a51486b1ad3e04cf297678f35aa7fb2281
marlonayalaunah <marlon.ayala@unah.hn>
2022-11-22 13:16:22 -0600

    Excel y PDF

--- src/components/generar_pdf/FormatoTablaPDF.js
+++ src/components/generar_pdf/FormatoTablaPDF.js
@@ -9,7 +9,7 @@ class FormatoTablaPDF extends React.Component {
 
   exportPDF = () => {
     const unit = "pt";
-    const size = "letter"; // Use A1, A2, A3 or A4
+    const size = "A4"; // Use A1, A2, A3 or A4
     const orientation = "portrait"; // portrait or landscape
 
     //const marginLeft = 40;
@@ -29,13 +29,28 @@ class FormatoTablaPDF extends React.Component {
 
     var sucursal = "Principal";
     var usuario = "jperez"
+    var fecha = "22-11-2022"
 
     var width = doc.internal.pageSize.getWidth() //Para centrar el texto
 
+
+
+
     doc.setFontSize(12);
-    doc.addImage(logo, 500, 10, 100, 50); // Agregar la imagen al PDF (X, Y, Width, Height)
-    doc.text(["Reporte de Categorias", "Del 1 al 31 de nobiembre de 2022", `Sucursal: ${sucursal}`, "Fecha: 21 de noviembre de 2022", `Usuario: ${usuario}`], width/2, 20, { align: 'center' });
+    doc.addImage(logo, 490, 10, 100, 50); // Agregar la imagen al PDF (X, Y, Width, Height)
+    doc.text(["Reporte de Categorias", "Del 1 al 31 de nobiembre de 2022", `Sucursal: ${sucursal}`, `Fecha: ${fecha}`, `Usuario: ${usuario}`], width/2, 30, { align: 'center' });
     doc.autoTable(tabla);
+
+    var pageCount = doc.internal.getNumberOfPages(); //Total Page Number
+    var i = 0;
+    for(i = 0; i < pageCount; i++) { 
+      doc.setPage(i); 
+      let pageCurrent = doc.internal.getCurrentPageInfo().pageNumber; //Current Page
+      doc.setFontSize(12);
+      doc.text('Pagina: ' + pageCurrent + ' de ' + pageCount, 10, doc.internal.pageSize.height - 10);
+      //doc.text('Pagina: ' + pageCurrent + ' de ' + pageCount, 210-20, 297-30, null, null);
+    }
+
     doc.save("Documento.pdf")
 
   }
