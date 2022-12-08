5510b0bbd221b2ac157fa346d3a37f5f6b56449e
CristhianOsorto <cosortoa@unah.hn>
2022-12-08 16:09:16 -0600

    Backup

--- src/components/seguridad/copia_seguridad/CopiaSeguridad.jsx
+++ src/components/seguridad/copia_seguridad/CopiaSeguridad.jsx
@@ -1,17 +1,105 @@
 import { Link } from "react-router-dom";
 import DataTable from "react-data-table-component";
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { Modal, ModalBody, ModalFooter, ModalHeader, Button } from "reactstrap";
 import { setGlobalState } from "../../../globalStates/globalStates";
 import Swal from "sweetalert2";
+import {Spinner} from "reactstrap";
 
+
+const UrlCrearCopia="http://190.53.243.69:3001/backup";
 const CopiaSeguridad = () => {
+  const [loading, setLoading] = useState(false);
+  const [copia, setcopia] = useState();
+
+  const navigate = useNavigate();
+
+
+//Alertas de éxito o error
+  const mostrarAlertas = (alerta) => {
+  switch (alerta) {
+    case "creado":
+      Swal.fire({
+        title: "¡Se subio el archivo con éxito!",
+        text: "Los archivos se subieron con éxito",
+        icon: "success",
+        confirmButtonColor: "#3085d6",
+        confirmButtonText: "Ok",
+      });
+
+      break;
+
+    case "error":
+      Swal.fire({
+        title: "Error",
+        text: "No se pudieron subir los archivos",
+        icon: "error",
+        confirmButtonColor: "#3085d6",
+        confirmButtonText: "Ok",
+      });
+      break;
+
+    default:
+      break;
+    }
+  };
+
   return (
-    <div>
-      <h3>Copia de Seguridad</h3>
-    </div>
+    <> 
+      {
+        loading?
+          <Modal size="sm" isOpen={loading} centered>
+            <ModalBody>
+             <div className="text-center">
+              <Spinner color="secondary" centered/>
+             </div>
+            </ModalBody>
+          </Modal>
+        
+        : 
+        <Formik>
+        onSubmit={async (valores) => {
+          setLoading(true)
+          //procedimineto para guardar el los cambios
+          try {
+            const res = await axios.get(UrlCrearCopia);
+            setcopia(res.data);
+            if (res.status === 200) {
+              setLoading(false)
+              mostrarAlertas("creado");
+              //InsertarBitacora(permisos[0].id_objeto, "EDITAR", "EDITAR DATOS);
+              //navigate("/admin/copiaseguridad");
+              //window.location.reload();
+            } else {
+              mostrarAlertas("error");
+            }
+          } catch (error) {
+            console.log(error);
+            mostrarAlertas("error");
+          }   
+        }}
+        
+          <Form>
+          <div>
+          <h3>Copia de Seguridad</h3>
+            <div className="col-sm-4">
+              <div className="mb-3">  
+                  <button className="btn btn-success mb-3 me-2" type="submit">
+                    Crear Copia de Seguridad
+                  </button>
+              </div>
+            </div>
+        </div>
+        </Form>
+        
+        
+      </Formik>
+      }
+      
+    </>    
   );
 };
 
--- src/components/seguridad/restauracion/Restauracion.jsx
+++ src/components/seguridad/restauracion/Restauracion.jsx
@@ -1,17 +1,197 @@
 import { Link } from "react-router-dom";
-import DataTable from "react-data-table-component";
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { Modal, ModalBody, ModalFooter, ModalHeader, Button } from "reactstrap";
-import { setGlobalState } from "../../../globalStates/globalStates";
+import axios from "axios";
 import Swal from "sweetalert2";
+import {Spinner} from "reactstrap";
+import { Modal, ModalBody, ModalFooter, ModalHeader, Button } from "reactstrap";
+
+const URLGuardar = "http://190.53.243.69:3001/backup";
+
+const objeto = "FORM_ARTICULO";
+
+
 
 const Restaurar = () => {
+  //const [DatosEmpresa] = useGlobalState('datosEmpresa');
+  const [loading, setLoading] = useState(false);
+  const [archivos, setarchivos] = useState("");
+  const subirArchivos =e =>{
+    setarchivos(e);
+  }
+
+  const navigate = useNavigate();
+
+  /*****Obtener y corroborar Permisos*****/
+ // const [img, setImg] = useState();
+
+
+  const [temp, setTemp] = useState([]);
+  const [permisos, setPermisos] = useState([]);
+  const [permitido, setPermitido] = useState(true);
+
+  const Permisos = () => {
+    const newData = temp.filter((item) => item.objeto === objeto);
+    setPermisos(newData);
+  };
+
+  useEffect(() => {
+    let data = localStorage.getItem("permisos");
+    if (data) {
+      setTemp(JSON.parse(data));
+    }
+  }, []);
+
+  useEffect(() => {
+    Permisos();
+  }, [temp]);
+
+  useEffect(() => {
+    if (permisos.length > 0) {
+      TienePermisos();
+    }
+  }, [permisos]);
+
+  const TienePermisos = () => {
+    setPermitido(permisos[0].permiso_consultar);
+  };
+  /*******************/
+
+  //Alertas de éxito o error
+  const mostrarAlertas = (alerta) => {
+    switch (alerta) {
+      case "subido":
+        Swal.fire({
+          title: "¡Se subio el archivo con éxito!",
+          text: "Los archivos se subieron con éxito",
+          icon: "success",
+          confirmButtonColor: "#3085d6",
+          confirmButtonText: "Ok",
+        });
+
+        break;
+
+      case "error":
+        Swal.fire({
+          title: "Error",
+          text: "No se pudieron subir los archivos",
+          icon: "error",
+          confirmButtonColor: "#3085d6",
+          confirmButtonText: "Ok",
+        });
+        break;
+
+      default:
+        break;
+    }
+  };
+
+
+
+  //Ventana modal de confirmación de eliminar
+ 
+
   return (
-    <div>
-      <h3>Restaurar Copia de Seguridad</h3>
-    </div>
+    <>
+    
+      {
+        loading?
+          <Modal size="sm" isOpen={loading} centered>
+            <ModalBody>
+             <div className="text-center">
+              <Spinner color="secondary" centered/>
+             </div>
+            </ModalBody>
+          </Modal>
+        
+        : 
+        <div className="container">
+        <Formik
+          
+          //Funcion para validar
+          validate={(valores) => {
+            let errores = {};
+
+           
+
+            return errores;
+          }}
+
+          onSubmit={async (valores) => {
+            setLoading(true)
+            //procedimineto para guardar el los cambios
+            try {
+              const res = await axios.post(URLGuardar);
+
+              if (res.status === 200) {
+                setLoading(false)
+                mostrarAlertas("subido");
+                //InsertarBitacora(permisos[0].id_objeto, "EDITAR", "EDITAR DATOS);
+                navigate("/admin/restaurar");
+                //window.location.reload();
+              } else {
+                mostrarAlertas("error");
+              }
+            } catch (error) {
+              console.log(error);
+              mostrarAlertas("error");
+            }
+
+
+              
+
+          }}
+        >
+          {({ errors, values }) => (
+            <Form>
+              <h3 className="mb-3">Restaurar Copia de Seguridad</h3>
+              <div className="row g-3">
+              <div className="row g-3">
+                <div className="col-sm-4">
+                  <div className="mb-3">
+                    <label htmlFor="Archivo" className="form-label">
+                      Subir Copia de Seguridad:
+                    </label>
+                    <input
+                      id="imgs"
+                      type="file"
+                      className="form-control"
+                      name="files"
+                      onChange={(e) => subirArchivos(e.target.files)}
+                    />
+                  </div>
+                </div>  
+
+                {/**AQUI OTRA */}
+              </div>
+
+                              
+              </div>
+<br />
+
+              <button className="btn btn-success mb-3 me-2" type="submit">
+                Subir Archivo
+              </button>
+              <Link
+                to="/admin/restaurar"
+                type="button"
+                className="btn btn-danger mb-3 me-2"
+              >
+                Cancelar
+              </Link>
+            </Form>
+          )}
+        </Formik>
+
+         
+
+      </div>
+      }
+
+
+      
+    </>
   );
 };
 
