4b978ef83b4befcb38ec77db87a2744ce2e4d086
eaplicano <eliud.aplicano@unah.hn>
2022-12-11 13:35:08 -0600

    periodocontable

--- src/components/contabilidad/periodocontable/MostrarPeriodoContable.jsx
+++ src/components/contabilidad/periodocontable/MostrarPeriodoContable.jsx
@@ -4,28 +4,26 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { Modal, ModalBody, ModalFooter, ModalHeader, Button } from "reactstrap";
 import { setGlobalState } from "../../../globalStates/globalStates";
-import Swal from "sweetalert2"; 
+import Swal from "sweetalert2";
 import { Export_PDF } from "./generarPDF/Export_PDF";
 import { useNavigate } from "react-router-dom";
 import { Export_Excel } from "./generarExcel/Export_Excel";
 import { RegistroEnVitacora } from "../../seguridad/bitacora/RegistroBitacora";
 
-
 const UrlMostrar = "http://190.53.243.69:3001/mc_periodo/getall";
 const UrlEliminar = "http://190.53.243.69:3001/mc_periodo/eliminar/";
 
-const objeto = "FORM_PERIODO_CONTABLE"
+const objeto = "FORM_PERIODO_CONTABLE";
 
-const MostrarPeriodoContable= () => {
+const MostrarPeriodoContable = () => {
   const navigate = useNavigate();
   //Configurar los hooks
-  const [registroDelete, setRegistroDelete] = useState('');
+  const [registroDelete, setRegistroDelete] = useState("");
   const [registros, setRegistros] = useState([]);
   useEffect(() => {
     getRegistros();
   }, []);
 
-  
   //procedimineto para obtener todos los registros
   const getRegistros = async () => {
     try {
@@ -37,23 +35,20 @@ const MostrarPeriodoContable= () => {
     }
   };
 
-  
   /*****Obtener y corroborar Permisos*****/
   const [temp, setTemp] = useState([]);
   const [permisos, setPermisos] = useState([]);
-  const [permitido, setPermitido] = useState(true)
+  const [permitido, setPermitido] = useState(true);
 
-  const Permisos = () =>{
-    const newData = temp.filter(
-      (item) => item.objeto === objeto
-    );
+  const Permisos = () => {
+    const newData = temp.filter((item) => item.objeto === objeto);
     setPermisos(newData);
-  }
+  };
 
   useEffect(() => {
-    let data = localStorage.getItem('permisos')
-    if(data){
-      setTemp(JSON.parse(data))
+    let data = localStorage.getItem("permisos");
+    if (data) {
+      setTemp(JSON.parse(data));
     }
   }, []);
 
@@ -61,110 +56,121 @@ const MostrarPeriodoContable= () => {
     Permisos();
   }, [temp]);
 
-
   useEffect(() => {
-    if(permisos.length > 0){
+    if (permisos.length > 0) {
       TienePermisos();
     }
   }, [permisos]);
 
+  const TienePermisos = () => {
+    setPermitido(permisos[0].permiso_consultar);
+  };
 
-  const TienePermisos = () =>{
-    setPermitido(permisos[0].permiso_consultar)
-  }
+  /*******************/
 
-/*******************/
+  //Alertas de éxito o error al eliminar
+  const mostrarAlertas = (alerta) => {
+    switch (alerta) {
+      case "eliminado":
+        Swal.fire({
+          title: "¡Eliminado!",
+          text: "El periodo contable se eliminó con éxito",
+          icon: "success",
+          confirmButtonColor: "#3085d6",
+          confirmButtonText: "Ok",
+        });
 
+        break;
 
-//Alertas de éxito o error al eliminar
-const mostrarAlertas = (alerta) =>{
-  switch (alerta){
-    case 'eliminado':
-      Swal.fire({
-        title: '¡Eliminado!',
-        text: "El periodo contable se eliminó con éxito",
-        icon: 'success',
-        confirmButtonColor: '#3085d6',
-        confirmButtonText: 'Ok'
-      });
+      case "error":
+        Swal.fire({
+          title: "Error",
+          text: "No se pudo eliminar el periodo contable",
+          icon: "error",
+          confirmButtonColor: "#3085d6",
+          confirmButtonText: "Ok",
+        });
 
-    break;
+        break;
 
-    case 'error':
-      Swal.fire({
-        title: 'Error',
-        text:  'No se pudo eliminar el periodo contable',
-        icon: 'error',
-        confirmButtonColor: '#3085d6',
-        confirmButtonText: 'Ok'
-      });
+      case "errormostrar":
+        Swal.fire({
+          title: "Error al Mostrar",
+          text: "En este momento no se pueden mostrar los datos, puede ser por un error de red o con el servidor. Intente más tarde.",
+          icon: "error",
+          confirmButtonColor: "#3085d6",
+          confirmButtonText: "Ok",
+        });
 
-    break;
+        break;
+      case "permisos":
+        Swal.fire({
+          title: "Lo siento, no tienes permisos para realizar esta acción.",
+          icon: "error",
+          confirmButtonColor: "#3085d6",
+          confirmButtonText: "Ok",
+        });
 
-    case 'errormostrar':
-      Swal.fire({
-        title: 'Error al Mostrar',
-        text:  'En este momento no se pueden mostrar los datos, puede ser por un error de red o con el servidor. Intente más tarde.',
-        icon: 'error',
-        confirmButtonColor: '#3085d6',
-        confirmButtonText: 'Ok'
-      });
+        break;
 
-    break;
-    case "permisos":
-      Swal.fire({
-        title: "Lo siento, no tienes permisos para realizar esta acción.",
-        icon: "error",
-        confirmButtonColor: "#3085d6",
-        confirmButtonText: "Ok",
-      });
-
-      break;
-
-    default: break;
-  }
-};
-
+      default:
+        break;
+    }
+  };
 
   //procedimineto para eliminar un registro
   const deleteRegistro = async () => {
     try {
-      console.log(registroDelete)
+      console.log(registroDelete);
       const res = await axios.delete(`${UrlEliminar}${registroDelete}`);
       getRegistros();
       if (res.status === 200) {
-         mostrarAlertas("eliminado"); 
-         RegistroEnVitacora(permisos[0].id_objeto, "ELIMINAR", "ELIMINAR PERIODO CONTABLE");
+        mostrarAlertas("eliminado");
+        RegistroEnVitacora(
+          permisos[0].id_objeto,
+          "ELIMINAR",
+          "ELIMINAR PERIODO CONTABLE"
+        );
       } else {
         mostrarAlertas("error");
-        RegistroEnVitacora(permisos[0].id_objeto, "ELIMINAR", "ERROR ELIMINAR PERIODO CONTABLE");
+        RegistroEnVitacora(
+          permisos[0].id_objeto,
+          "ELIMINAR",
+          "ERROR ELIMINAR PERIODO CONTABLE"
+        );
       }
     } catch (error) {
       console.log(error);
       mostrarAlertas("error");
-      RegistroEnVitacora(permisos[0].id_objeto, "ELIMINAR", "ERROR ELIMINAR PERIODO CONTABLE");
+      RegistroEnVitacora(
+        permisos[0].id_objeto,
+        "ELIMINAR",
+        "ERROR ELIMINAR PERIODO CONTABLE"
+      );
     }
   };
 
   //Barra de busqueda
-    const [ busqueda, setBusqueda ] = useState("")
-      //capturar valor a buscar
-    const valorBuscar = (e) => {
-      setBusqueda(e.target.value)   
-  }
-      //metodo de filtrado 
-  let results = []
-   if(!busqueda){
-       results = registros
-   }else{
-        results = registros.filter( (dato) =>
-        dato.fecha_inicial.toString().includes(busqueda.toLocaleLowerCase()) || 
+  const [busqueda, setBusqueda] = useState("");
+  //capturar valor a buscar
+  const valorBuscar = (e) => {
+    setBusqueda(e.target.value);
+  };
+  //metodo de filtrado
+  let results = [];
+  if (!busqueda) {
+    results = registros;
+  } else {
+    results = registros.filter(
+      (dato) =>
+        dato.fecha_inicial.toString().includes(busqueda.toLocaleLowerCase()) ||
         dato.fecha_final.toString().includes(busqueda.toLocaleLowerCase()) ||
-        dato.descripcion_periodo.toLowerCase().includes(busqueda.toLocaleLowerCase())
-        )
-   };
+        dato.descripcion_periodo
+          .toLowerCase()
+          .includes(busqueda.toLocaleLowerCase())
+    );
+  }
 
-    
   //Ventana modal de confirmación de eliminar
   const [modalEliminar, setModalEliminar] = useState(false);
   const abrirModalEliminar = () => setModalEliminar(!modalEliminar);
@@ -172,7 +178,7 @@ const mostrarAlertas = (alerta) =>{
   //Ventana modal para mostrar mas
   const [modalVerMas, setVerMas] = useState(false);
   const abrirModalVerMas = () => setVerMas(!modalVerMas);
-  const [periodoVerMas, setPeriodoVerMas] = useState({});  
+  const [periodoVerMas, setPeriodoVerMas] = useState({});
 
   //Configuramos las columnas de la tabla
   const columns = [
@@ -187,14 +193,19 @@ const mostrarAlertas = (alerta) =>{
       sortable: true,
     },
     {
-        name: "FECHA INICIAL",
-        selector: (row) => row.fecha_inicial,
-        sortable: true,
+      name: "TIPO",
+      selector: (row) => row.descripcion_tipo_periodo,
+      sortable: true,
     },
     {
-        name: "FECHA FINAL",
-        selector: (row) => row.fecha_final,
-        sortable: true,
+      name: "FECHA INICIAL",
+      selector: (row) => row.fecha_inicial,
+      sortable: true,
+    },
+    {
+      name: "FECHA FINAL",
+      selector: (row) => row.fecha_final,
+      sortable: true,
     },
     /*{
         name: "FECHA DE CREACIÓN",
@@ -207,11 +218,15 @@ const mostrarAlertas = (alerta) =>{
         sortable: true,
     },*/
     {
-        name: "NOMBRE USUARIO",
-        selector: (row) => row.nombre_usuario,
-        sortable: true,
+      name: "NOMBRE USUARIO",
+      selector: (row) => row.nombre_usuario,
+      sortable: true,
     },
-    
+    {
+      name: "ESTADO",
+      selector: (row) => row.descripcion_estado_periodo,
+      sortable: true,
+    },
 
     {
       name: "ACCIONES",
@@ -224,28 +239,31 @@ const mostrarAlertas = (alerta) =>{
             onClick={() => {
               abrirModalVerMas();
               setPeriodoVerMas(row);
-              RegistroEnVitacora(permisos[0].id_objeto, "LECTURA", "MOSTRAR MAS PERIODO CONTABLE");
+              RegistroEnVitacora(
+                permisos[0].id_objeto,
+                "LECTURA",
+                "MOSTRAR MAS PERIODO CONTABLE"
+              );
             }}
           >
-             <i className="bi bi-eye-fill"></i>
+            <i className="bi bi-eye-fill"></i>
           </Link>
           &nbsp;
-            <button
+          <button
             type="button"
             className="btn btn-light"
             title="Editar"
             onClick={() => {
-              if(permisos[0].permiso_actualizacion){
+              if (permisos[0].permiso_actualizacion) {
                 setGlobalState("registroEdit", row);
-                navigate("/admin/editarperiodo")
-              }else{
+                navigate("/admin/editarperiodo");
+              } else {
                 mostrarAlertas("permisos");
-              }              
+              }
             }}
           >
             <i className="bi bi-pencil-square"></i>
           </button>
-         
           {/*</Link>
           &nbsp;          
           <Link
@@ -262,13 +280,12 @@ const mostrarAlertas = (alerta) =>{
             className="btn btn-light"
             title="Eliminar"
             onClick={() => {
-              if(permisos[0].permiso_eliminacion){
+              if (permisos[0].permiso_eliminacion) {
                 setRegistroDelete(row.id_periodo_contable);
                 abrirModalEliminar();
-              }else{
+              } else {
                 mostrarAlertas("permisos");
               }
-              
             }}
           >
             <i className="bi bi-trash3-fill"></i>
@@ -289,43 +306,42 @@ const mostrarAlertas = (alerta) =>{
     selectAllRowsItemText: "Todos",
   };
 
-  return (    
+  return (
     <div className="container">
       <h3>Periodo Contable</h3>
       <br />
 
-      {permitido? (
-     
-     <div>
-      {/*Mostrar los botones: Nuevo, Excel y PDF */}
-      <div className="row">
-        <div className="col">
-          <div
-            className="btn-toolbar"
-            role="toolbar"
-            aria-label="Toolbar with button groups"
-          >
-            <div
-              className="btn-group me-2"
-              role="group"
-              aria-label="First group"
-            >
-              <button
-           type="button"
-           className="btn btn-primary"
-           title="Agregar Nuevo"
-           onClick={() => {
-             if(permisos[0].permiso_insercion){
-               navigate("/admin/crearperiodo")
-             }else{
-              mostrarAlertas("permisos");
-             }              
-           }}
-         >
-           <i className="bi bi-plus-lg"></i> Nuevo
-         </button>
-             
-             {/*} <Link
+      {permitido ? (
+        <div>
+          {/*Mostrar los botones: Nuevo, Excel y PDF */}
+          <div className="row">
+            <div className="col">
+              <div
+                className="btn-toolbar"
+                role="toolbar"
+                aria-label="Toolbar with button groups"
+              >
+                <div
+                  className="btn-group me-2"
+                  role="group"
+                  aria-label="First group"
+                >
+                  <button
+                    type="button"
+                    className="btn btn-primary"
+                    title="Agregar Nuevo"
+                    onClick={() => {
+                      if (permisos[0].permiso_insercion) {
+                        navigate("/admin/crearperiodo");
+                      } else {
+                        mostrarAlertas("permisos");
+                      }
+                    }}
+                  >
+                    <i className="bi bi-plus-lg"></i> Nuevo
+                  </button>
+
+                  {/*} <Link
                 to="/admin/crearperiodo"
                 type="button"
                 className="btn btn-primary"
@@ -333,81 +349,88 @@ const mostrarAlertas = (alerta) =>{
               >
                 <i className="fa-solid fa-plus"></i> Nuevo
       </Link>*/}
+                </div>
+                <div
+                  className="btn-group me-2"
+                  role="group"
+                  aria-label="Second group"
+                >
+                  <Button
+                    to="/"
+                    type="button"
+                    className="btn btn-success"
+                    title="Exportar a Excel"
+                    onClick={() => {
+                      Export_Excel(results);
+                      RegistroEnVitacora(
+                        permisos[0].id_objeto,
+                        "EXPORTAR",
+                        "EXPORTAR EXCEL PERIODO CONTABLE"
+                      );
+                    }}
+                  >
+                    <i className="bi bi-file-earmark-excel-fill"></i>
+                  </Button>
+                  <Button
+                    type="button"
+                    className="btn btn-danger"
+                    title="Exportar a PDF"
+                    onClick={() => {
+                      Export_PDF(results);
+                      RegistroEnVitacora(
+                        permisos[0].id_objeto,
+                        "EXPORTAR",
+                        "EXPORTAR PDF PERIODO CONTABLE"
+                      );
+                    }}
+                  >
+                    <i className="bi bi-filetype-pdf"></i>
+                  </Button>
+                </div>
+              </div>
             </div>
-            <div
-              className="btn-group me-2"
-              role="group"
-              aria-label="Second group"
-            >
-              <Button
-                to="/"
-                type="button"
-                className="btn btn-success"
-                title="Exportar a Excel"
-                onClick={()=>{
-                  Export_Excel(results);
-                  RegistroEnVitacora(permisos[0].id_objeto, "EXPORTAR", "EXPORTAR EXCEL PERIODO CONTABLE");
-                }}
-              >
-                <i className="bi bi-file-earmark-excel-fill"></i>
-              </Button>
-              <Button
-                type="button"
-                className="btn btn-danger"
-                title="Exportar a PDF"
-                onClick={() =>{
-                  Export_PDF(results);
-                  RegistroEnVitacora(permisos[0].id_objeto, "EXPORTAR", "EXPORTAR PDF PERIODO CONTABLE");
-                }}
-              >
-                <i className="bi bi-filetype-pdf"></i>
-              </Button>
+
+            {/*Mostrar la barra de busqueda*/}
+            <div className="col-4">
+              <div className="input-group flex-nowrap">
+                <span className="input-group-text" id="addon-wrapping">
+                  <i className="bi bi-search"></i>
+                </span>
+                <input
+                  className="form-control me-2"
+                  type="text"
+                  placeholder="Buscar por descripción, fecha inicial/fecha final..."
+                  aria-label="Search"
+                  value={busqueda}
+                  onChange={valorBuscar}
+                />
+              </div>
             </div>
           </div>
-        </div>
+          <br />
 
-        {/*Mostrar la barra de busqueda*/}
-        <div className="col-4">
-          <div className="input-group flex-nowrap">
-            <span className="input-group-text" id="addon-wrapping">
-              <i className="bi bi-search"></i>
-            </span>
-            <input
-              className="form-control me-2"
-              type="text"
-              placeholder="Buscar por descripción, fecha inicial/fecha final..."
-              aria-label="Search"
-              value={busqueda}
-              onChange={valorBuscar}
-            />
+          {/*Mostramos la tabla con los datos*/}
+          <div className="row">
+            {results.length > 0 ? (
+              <DataTable
+                columns={columns}
+                data={results}
+                pagination
+                paginationComponentOptions={paginationComponentOptions}
+                highlightOnHover
+                fixedHeader
+                fixedHeaderScrollHeight="550px"
+              />
+            ) : (
+              <p className="text-center">Ninguna Categoría</p>
+            )}
           </div>
         </div>
-      </div>
-      <br />
-
-      {/*Mostramos la tabla con los datos*/}
-      <div className="row">
-      {results.length > 0 ? (
-        <DataTable
-          columns={columns}
-          data={results}
-          pagination
-          paginationComponentOptions={paginationComponentOptions}
-          highlightOnHover
-          fixedHeader
-          fixedHeaderScrollHeight="550px"
-        />
-        ) : (
-          <p className="text-center">Ninguna Categoría</p>
-        )}
-      </div>
-         </div>
-     
-     ) : (
-       <p className="text-center text-danger">Lo siento, no tienes permisos para realizar esta acción.</p>
-     )}
-     
-     
+      ) : (
+        <p className="text-center text-danger">
+          Lo siento, no tienes permisos para realizar esta acción.
+        </p>
+      )}
 
       {/* Ventana Modal de ver más*/}
       <Modal isOpen={modalVerMas} toggle={abrirModalVerMas} centered>
@@ -472,7 +495,7 @@ const mostrarAlertas = (alerta) =>{
             Cerrar
           </Button>
         </ModalFooter>
-      </Modal>      
+      </Modal>
 
       {/* Ventana Modal de Eliminar*/}
       <Modal isOpen={modalEliminar} toggle={abrirModalEliminar} centered>
@@ -495,7 +518,6 @@ const mostrarAlertas = (alerta) =>{
           </Button>
         </ModalFooter>
       </Modal>
-
     </div>
   );
 };
